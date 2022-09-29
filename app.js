c716f546193cad532662bbcea450906c6af59cfd
Danil <danil.kersontsev@khk.ee>
2022-09-29 11:16:16 +0300

    As a user i can delete single task[deliver #183413656]

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const form = document.querySelector('form')
-const taskinput = document.querySelector('#task')
-form.addEventListener('submit', addtask)
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
 
-function addtask(e){
-    console.log(taskinput.value)
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+
+function addTask(e){
+    // create list item
     const li = document.createElement('li')
-    li.appendChild(document.createTextNode(taskinput.value))
+    li.appendChild(document.createTextNode(taskInput.value))
     li.className = 'collection-item'
     const a = document.createElement('a')
-    a.appendChild(document.createTextNode('x'))
-    a.className = "blue-text text-darken-2 secondary-content"
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
     a.setAttribute('href', '#')
     li.appendChild(a)
-    console.log(li)
+    // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
-    taskinput.value = ""
+    taskInput.value = ''
     e.preventDefault()
+}
+
+function deleteTask(e){
+    if(e.target.textContent === 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            e.target.parentElement.remove()
+        }
+    }
 }
